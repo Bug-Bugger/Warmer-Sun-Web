df0929ab99c634ca53e050e500794ec20e3b1f64
Huajie Zhong <hz642@cornell.edu>
2024-03-10 03:29:47 -0400

    fix font size

--- warmersun/pages/map.vue
+++ warmersun/pages/map.vue
@@ -23,7 +23,7 @@
         <l-popup>
           <h1>{{ spot.park }}</h1>
           <p>{{ spot.name }}</p>
-          <a :href="'/parks/spots/' + spot.id">More details</a>
+          <a :href="'/parks/spots/' + spot.id" style="font-size: small;">More details</a>
         </l-popup>
         </l-marker>
         </l-map>
